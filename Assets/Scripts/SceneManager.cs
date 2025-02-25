b87efdeed7a4fa7cf9cfd179878cada56588c957
Juan Pablo Resendes Rosas <resendes.rosas614@gmail.com>
2025-02-24 20:51:43 -0600

    Segundo Avance
    SkyDome
    Cambio de Escena
    Mobiles y Desktop
    Fades

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
